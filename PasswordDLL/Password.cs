28f45076cf2f31bfb6e704285a203d8f2c7b0d9c
someverything <rustameyvazov@icloud.com>
2025-01-26 00:57:06 +0400

    Add project files.

---
+++ PasswordDLL/Password.cs
@@ -0,0 +1,43 @@
+﻿namespace PasswordDLL
+{
+    public class Password
+    {
+        //attributes
+        public string PasswordValue { get; private set; }   
+        public int Length => PasswordValue.Length;
+        public int DigitCount => PasswordValue.Count(char.IsDigit);
+
+        //ctor that initializes empty password
+        public Password()
+        {
+            PasswordValue = string.Empty;
+        }
+
+        //Generates Password
+        public void GeneratePassword(int length = 8)
+        {
+            var random = new Random();
+            const string validChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()_-";
+            PasswordValue = new string(Enumerable.Range(0, length).Select(_ => validChars[random.Next(validChars.Length)]).ToArray());
+        }
+
+        //Returns Password
+        public string GetPassword()
+        {
+            return PasswordValue;
+        }
+
+        // Return PasswordLength
+        public int GetPasswordLength()
+        {
+            return Length;
+        }
+
+        // Returns PasswordDigitCount
+        public int GetPasswordDigitCount()
+        {
+            return DigitCount;
+        }
+
+    }
+}
